e2ab04f7e7d352cd026c1ca74f144531bf109594
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-15 17:45:16 -0500

    Add petition url

--- Project-7-whitehouse-petitions/ViewController.swift
+++ Project-7-whitehouse-petitions/ViewController.swift
@@ -9,13 +9,32 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var petitions = [String]()
+    var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Whitehouse News"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url){
+                parse(json: data)
+            }
+        }
+        
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,11 +43,39 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
         var content = cell.defaultContentConfiguration()
-        content.text = "Title"
-        content.secondaryText = "Subtitle"
+        
+        content.text = petition.title
+        content.secondaryText = String(petition.body.prefix(100)) + "..."
         cell.contentConfiguration = content
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        
+        let shareAction = UIAction(
+            title: "Share",
+            image: UIImage(systemName: "square.and.arrow.up"),
+            identifier: nil
+        ) { _ in
+            print("hello")
+        }
+        
+        let deleteAction = UIAction(
+            title: "Delete",
+            image: UIImage(systemName: "trash.fill"),
+            identifier: nil,
+            attributes: .destructive
+        ) { _ in
+        }
+        
+        let menu = UIMenu(title: "Menu", children: [shareAction, deleteAction] )
+        
+        return UIContextMenuConfiguration(identifier: nil, actionProvider:  { _ in
+            return menu
+        })
+        
     }
 
 
