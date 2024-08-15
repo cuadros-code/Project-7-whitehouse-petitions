8f342e35f33dd87da085c817ded0fa5a7c3f022e
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-15 12:20:51 -0500

    Add table, TabBarController

--- Project-7-whitehouse-petitions/ViewController.swift
+++ Project-7-whitehouse-petitions/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Whitehouse News"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Title"
+        content.secondaryText = "Subtitle"
+        cell.contentConfiguration = content
+        return cell
     }
 
 
